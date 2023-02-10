bf9b2c46288a6e170e84c1a666ae56d2181388db
JuliaUvarova <yuliasennikova@yandex.ru>
2023-02-10 16:14:18 +0300

    Created new array with 3&less-chars-elements

--- GB_FirstProject/Program.cs
+++ GB_FirstProject/Program.cs
@@ -20,5 +20,51 @@ string[] StringArray(string message)
     return words;
 }
 
-string[] words = StringArray
-(ReadDataString("Введите через запятую слюбые строки: "));
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод считает число строк нужной длины
+int CountLines(string[] arr,int maxSize)
+{
+    int countLines =0;
+    for(int i=0;i<arr.Length; i++)
+    {
+        if(arr[i].Length<=maxSize)
+        {
+            countLines=countLines+1;
+        }
+    }
+    return countLines;
+}
+
+//Метод включает в массив только строки определенной длины
+string[] ReducedArray(string[] arr, int countLines, int maxSize)
+{
+    string[] newArr = new string[countLines];
+    int position=0;
+        for(int j=0; j<arr.Length; j++)
+        {
+            if(arr[j].Length<=maxSize)
+            {
+                newArr[position] = arr[j];
+                position = position+1;
+            }
+        }
+        return newArr;
+}
+
+
+
+string[] words = StringArray(ReadDataString("Введите через запятую слюбые строки: "));
+Console.WriteLine();
+int maxSize = ReadData("Введите максимальное число символов, которое должна содержать строка нового массива: ");
+int countLines = CountLines(words, maxSize);
+string[] newArr = ReducedArray(words, countLines, maxSize);
+Console.WriteLine();
+
+
+
