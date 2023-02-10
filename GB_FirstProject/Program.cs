db531330fad1e5421875bd87f479faa3663eeef4
JuliaUvarova <yuliasennikova@yandex.ru>
2023-02-10 12:41:15 +0300

    Created string array

---
+++ GB_FirstProject/Program.cs
@@ -0,0 +1,24 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует
+//массив строк, не превышающих в длину трех символов. 
+
+//Метод читает и возвращает строку
+string ReadDataString(string msg)
+{
+    Console.WriteLine(msg);
+    string index = Console.ReadLine() ?? "0";
+    return index;
+}
+
+//Метод преобразует строку в строковой массив
+string[] StringArray(string message)
+{
+    string[] words = message.Split(',');
+    foreach (var word in words)
+    {
+        System.Console.WriteLine($"{word}");
+    }
+    return words;
+}
+
+string[] words = StringArray
+(ReadDataString("Введите через запятую слюбые строки: "));
